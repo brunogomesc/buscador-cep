4eafb0e2b27f1abedb311ca2eed12e24433d7dd0
Bruno Costa <brunogomesc@outlook.com.br>
2022-11-24 18:39:44 -0300

    Criando projeto de consulta de cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+import { useState } from 'react'
+import api from './services/api'
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+    if(input === '') {
+      alert("Preencha algum CEP!");
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput("")
+    } catch (error) {
+      alert("Ops! Erro ao buscar CEP!")
+      setInput("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input 
+          type="text" 
+          placeholder="Digite seu CEP..." 
+          value={input} 
+          onChange={(event)=>{setInput(event.target.value)}}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFFF"/>
+        </button>
+      </div>
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          {
+            cep.cep === "" /* Condição */
+            ? <h2>CEP: - </h2> /* Se for verdadeira */
+            : <h2>CEP: {cep.cep}</h2> /* Se for falsa */
+          }
+          {
+            cep.logradouro === "" /* Condição */
+            ? <span>Logradouro: - </span> /* Se for verdadeira */
+            : <span>Logradouro: {cep.logradouro}</span> /* Se for falsa */
+          }
+          {
+            cep.complemento === "" /* Condição */
+            ? <span>Complemento: - </span> /* Se for verdadeira */
+            : <span>Complemento: {cep.complemento}</span> /* Se for falsa */
+          }
+          {
+            cep.bairro === "" /* Condição */
+            ? <span>Bairro: - </span> /* Se for verdadeira */
+            : <span>Bairro: {cep.bairro}</span> /* Se for falsa */
+          }
+          {
+            cep.localidade === "" /* Condição */
+            ? <span>Localidade: - </span> /* Se for verdadeira */
+            : <span>Localidade: {cep.localidade}</span> /* Se for falsa */
+          }
+        </main>
+      )}
     </div>
   );
 }
